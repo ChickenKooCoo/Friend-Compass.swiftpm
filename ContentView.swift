9fd33d2e329b9ca491a7e842eb214390c446009b
Wenglo8 <aaj8fujiwara@gmail.com>
2025-04-28 14:20:34 -0500

    m

--- ContentView.swift
+++ ContentView.swift
@@ -3,14 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Text("Balls") + Text("Torture")
-            Text("repo")
-            Rectangle()
-                .background(Circle())
+            
         }
     }
 }
---
+++ settings.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Friend Compass
+//
+//  Created by Aaron J. Fujiwara on 4/28/25.
+//
+
+import Foundation
+import SwiftUI
